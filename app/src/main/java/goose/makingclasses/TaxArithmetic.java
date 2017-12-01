de487f25b89edea6c449945304f1a77180b53b8d
Gustavo Silvera <31013486+SilveraGus@users.noreply.github.com>
2017-12-01 10:28:29 -0800

    Update TaxArithmetic.java

--- app/src/main/java/goose/makingclasses/TaxArithmetic.java
+++ app/src/main/java/goose/makingclasses/TaxArithmetic.java
@@ -5,10 +5,11 @@ package goose.makingclasses;
  */
 
 public class TaxArithmetic extends Arithmetic {
-    public float calculateTax(float purchase, float Trate) {
+    //utilizes the steps for subclasses and tax calculations
+    public float calculateTax(float purchase, float Trate) {//calculates the tax given the purchase
         return mult(purchase, div(Trate, (float)100));//purchase * Trate /100
     }
-    public float calculateTax(float Trate) {
+    public float calculateTax(float Trate) {//calculates the tax with no purchase
         float purchase = getm1() + getm2();//using public instances
         return calculateTax(purchase, Trate);
     }
