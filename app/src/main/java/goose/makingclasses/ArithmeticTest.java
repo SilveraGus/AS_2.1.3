d216743626bcb40b60f9851a848a212ac0458e23
Gustavo Silvera <31013486+SilveraGus@users.noreply.github.com>
2017-12-01 10:25:11 -0800

    Update ArithmeticTest.java

--- app/src/main/java/goose/makingclasses/ArithmeticTest.java
+++ app/src/main/java/goose/makingclasses/ArithmeticTest.java
@@ -7,28 +7,33 @@ import junit.framework.TestCase;
 public class ArithmeticTest extends TestCase{
 
     Arithmetic a;
-    private float m1 = 5;
-    private float m2 = 10;
+    private float m1 = 5;//one test value for the checks
+    private float m2 = 10;//one test value for the checks
     @Override
     public void setUp()throws Exception{
         super.setUp();
-        a = new Arithmetic();
+        a = new Arithmetic();//creates a new arithmetic class
     }
 
     public void testArithmeticAdd() {
+        //tests the arithmetic add function
         assertEquals(a.add(m1, m2), (float)15);
     }
     public void testArithmeticMult() {
+        //tests the arithmetic multiplication function
         assertEquals(a.mult(m1, m2), (float)50);
     }
     public void testArithmeticDiff() {
+        //tests the arithmetic differene function
         assertEquals(a.diff(m1, m2), (float)(-5));
     }
     public void testArithmeticDiv() {
+        //tests the arithmetic division function
         assertEquals(a.div(m1, m2), (float)0.5);
     }
     TaxArithmetic taxes =  new TaxArithmetic();
     public void testArithmeticTax() {
+        //tests the arithmetic tax calfulator function
         assertEquals(taxes.calculateTax((float)10.75, (float)8.5), (float)0.91375);
     }
     @Override
